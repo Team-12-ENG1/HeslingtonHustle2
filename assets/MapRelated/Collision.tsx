1ea24769df739345eeeaff07a0e43751baf6a22c
Daniel Agar <da1098@york.ac.uk>
2024-04-17 14:27:05 +0100

    Improved collisions on map

---
+++ assets/MapRelated/Collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Collision" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="X.png" width="16" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
