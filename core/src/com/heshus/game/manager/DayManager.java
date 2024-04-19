a55ac5cd02437d0d4edada93659e1ec337b0acb2
Mxtt-smith <117466868+Mxtt-smith@users.noreply.github.com>
2024-04-19 14:57:23 +0100

    Added constructor
    Next job is find everywhere they've instantiated dayManager to make it work  with my changes

--- core/src/com/heshus/game/manager/DayManager.java
+++ core/src/com/heshus/game/manager/DayManager.java
@@ -1,27 +1,37 @@
 package com.heshus.game.manager;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 /**
  * Manages how the current day is changed and whether the game has finished
  */
 public class DayManager {
-    public static Day currentDay = new Day(1, 8, 100);
+    public static Day currentDay;
     public static boolean gameOver = false;
     public static int overallEatScore = 0;
     public static int overallStudyScore = 0;
     public static int overallRecreationalScore = 0;
 
+    public static Dictionary<Integer,Dictionary<String,Integer>> statsByDay;
+
+    public DayManager(){
+        currentDay = new Day(1, 8, 100);
+        statsByDay = new Hashtable<Integer,Dictionary<String,Integer>>();
+    }
     /**
      * Controls what happens at the end of the day
      * If the current day is less than 7 then reset relevant variables
      * If the current day is 7 or greater, the game is over
      */
     public static void incrementDay(){
-        if(currentDay.getDayNumber() < 7){
-            //Change day number
-            currentDay.incrementDayNumber();
-            //Reset variables for a new "day"
-            currentDay.resetTime();
-            currentDay.resetEnergy();
+        if(currentDay.getDayNumber() <= 7){
+            int dayNum = currentDay.getDayNumber();
+            statsByDay.put(dayNum, currentDay.summariseDay());
+            overallEatScore += currentDay.getEatScore();
+            overallRecreationalScore += currentDay.getRecreationalScore();
+            overallStudyScore += currentDay.getStudyScore();
+            currentDay = new Day(dayNum++,8,100);
         }
         else{
             gameOver = true;
