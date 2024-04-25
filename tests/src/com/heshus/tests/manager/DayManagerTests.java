388c590b5ce2ce4ae82a8af6d61cabb486b4ffaf
asparagus2 <160022380+asparagus2@users.noreply.github.com>
2024-04-25 14:42:32 +0100

    Update DayManagerTests.java

---
+++ tests/src/com/heshus/tests/manager/DayManagerTests.java
@@ -0,0 +1,33 @@
+package com.heshus.tests.manager;
+
+import com.heshus.game.manager.Day;
+import com.heshus.game.manager.DayManager;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class DayManagerTests {
+
+    //tests that the day counter increments when the method is called
+    @Test
+    public void dayIncrements() {
+        DayManager dm = new DayManager();
+        int prevDayNum = dm.currentDay.getDayNumber();
+        dm.incrementDay();
+        assertEquals(prevDayNum + 1, dm.currentDay.getDayNumber());
+
+    }
+
+    //tests that gameover is set to true after the 7th day
+    @Test
+    public void gameOverAfterLastDay(){
+        DayManager dm = new DayManager();
+        for(int i = 0; i<7;i++){
+            dm.incrementDay();
+        }
+        assertFalse(dm.getGameOver());
+        dm.incrementDay();
+        assertTrue(dm.getGameOver());
+    }
+}
