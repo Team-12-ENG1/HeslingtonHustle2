fcedb9cafb86ee27f273b39818acdd232d3384af
asparagus2 <160022380+asparagus2@users.noreply.github.com>
2024-05-01 12:36:03 +0100

    Fixed asset tests

--- tests/src/com/heshus/tests/assets/AssetTests.java
+++ tests/src/com/heshus/tests/assets/AssetTests.java
@@ -2,6 +2,13 @@ package com.heshus.tests.assets;
 
 import static org.junit.Assert.assertTrue;
 
+import com.heshus.game.editor.CustomiseSprite;
+import com.heshus.game.engine.Play;
+import com.heshus.game.manager.Save;
+import com.heshus.game.screens.states.LeaderboardScreen;
+import com.heshus.game.screens.states.MainMenuScreen;
+import com.heshus.game.screens.states.PauseMenu;
+import com.heshus.game.screens.states.PlayerNameScreen;
 import com.heshus.tests.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,26 +19,108 @@ import com.badlogic.gdx.Gdx;
 public class AssetTests {
 
     @Test
-    public void CustomiseSpritesAssets() {
-        assertTrue("");
-        public static final String CLICK_SOUND = "Sounds/switch2.ogg";
-        public static final String FONT = "Fonts/monogram/pixel.fnt";
-        public static final String LEFT_ARROW = "UI/keyboard_arrow_left_outline.png" ;
-        public static final String RIGHT_ARROW = "UI/keyboard_arrow_right_outline.png" ;
+    public void customiseSpritesAssets() {
+        assertTrue("Passes when CLICK_SOUND is present",
+                Gdx.files.internal(CustomiseSprite.CLICK_SOUND).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(CustomiseSprite.FONT).exists());
+        assertTrue("Passes when LEFT_ARROW is present",
+                Gdx.files.internal(CustomiseSprite.LEFT_ARROW).exists());
+        assertTrue("Passes when RIGHT_ARROW is present",
+                Gdx.files.internal(CustomiseSprite.RIGHT_ARROW).exists());
+        assertTrue("Passes when LEFT_ARROW is present",
+                Gdx.files.internal(CustomiseSprite.LEFT_ARROW).exists());
     }
 
     @Test
-    public void EatExists(){
-        assertTrue("Passes when eat.png is present",
-                Gdx.files.internal("../assets/expandedMap/Eat.png").exists());
-        assertTrue("Passes when EatTile.png exists",
-                Gdx.files.internal("../assets/expandedMap/EatTile.png").exists());
+    public void playAssets(){
+        assertTrue("Passes when MAP is present",
+                Gdx.files.internal(Play.MAP).exists());
+        assertTrue("Passes when WHITE_SQUARE is present",
+                Gdx.files.internal(Play.WHITE_SQUARE).exists());
+        assertTrue("Passes when TEXT_BUBBLE is present",
+                Gdx.files.internal(Play.TEXT_BUBBLE).exists());
+        assertTrue("Passes when COUNTER_BOX is present",
+                Gdx.files.internal(Play.COUNTER_BOX).exists());
+        assertTrue("Passes when VERTICAL_BAR is present",
+                Gdx.files.internal(Play.VERTICAL_BAR).exists());
+
+        assertTrue("Passes when TILE_1 is present",
+                Gdx.files.internal(Play.TILE_1).exists());
+        assertTrue("Passes when TILE_2 is present",
+                Gdx.files.internal(Play.TILE_2).exists());
+        assertTrue("Passes when TILE_3 is present",
+                Gdx.files.internal(Play.TILE_3).exists());
+        assertTrue("Passes when TILE_4 is present",
+                Gdx.files.internal(Play.TILE_4).exists());
+
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(Play.CLICK).exists());
+        assertTrue("Passes when VOLUME_UP is present",
+                Gdx.files.internal(Play.VOLUME_UP).exists());
+        assertTrue("Passes when VOLUME_DOWN is present",
+                Gdx.files.internal(Play.VOLUME_DOWN).exists());
     }
 
     @Test
-    public void CollisionFileExists(){
-        assertTrue("Passes when Collision.tsx is present",
-                Gdx.files.internal("../assets/expandedMap/Collision.tsx").exists());
+    public void saveAssets(){
+        assertTrue("Passes when SCORES is present",
+                Gdx.files.internal(Save.SCORES).exists());
     }
 
+    @Test
+    public void leaderBoardAssets(){
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(LeaderboardScreen.CLICK).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(LeaderboardScreen.FONT).exists());
+        assertTrue("Passes when BUTTON is present",
+                Gdx.files.internal(LeaderboardScreen.BUTTON).exists());
+    }
+
+    @Test
+    public void mainMenuAssets(){
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(MainMenuScreen.CLICK).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(MainMenuScreen.FONT).exists());
+        assertTrue("Passes when BUTTON is present",
+                Gdx.files.internal(MainMenuScreen.BUTTON).exists());
+        assertTrue("Passes when BUTTON_WIDE is present",
+                Gdx.files.internal(MainMenuScreen.BUTTON_WIDE).exists());
+    }
+
+    @Test
+    public void pauseMenuAssets(){
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(PauseMenu.CLICK).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(PauseMenu.FONT).exists());
+        assertTrue("Passes when BUTTON is present",
+                Gdx.files.internal(PauseMenu.BUTTON).exists());
+    }
+
+    @Test
+    public void playerNameAssets(){
+        assertTrue("Passes when SKIN is present",
+                Gdx.files.internal(PlayerNameScreen.SKIN).exists());
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(PlayerNameScreen.CLICK).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(PlayerNameScreen.FONT).exists());
+        assertTrue("Passes when BUTTON is present",
+                Gdx.files.internal(PlayerNameScreen.BUTTON).exists());
+    }
+    
+    @Test
+    public void settingsAssets(){
+        assertTrue("Passes when CLICK is present",
+                Gdx.files.internal(PlayerNameScreen.CLICK).exists());
+        assertTrue("Passes when FONT is present",
+                Gdx.files.internal(PlayerNameScreen.FONT).exists());
+        assertTrue("Passes when BUTTON is present",
+                Gdx.files.internal(PlayerNameScreen.BUTTON).exists());
+
+
+    }
 }
