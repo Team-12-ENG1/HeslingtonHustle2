b8bb01377495e9f017139aa6f12977862959e99a
asparagus2 <160022380+asparagus2@users.noreply.github.com>
2024-05-19 15:15:48 +0100

    fixed diagonal movement tests

--- tests/src/com/heshus/tests/entities/PlayerTests.java
+++ tests/src/com/heshus/tests/entities/PlayerTests.java
@@ -66,9 +66,9 @@ public class PlayerTests {
         player.update(0.01f);
         Vector2 vel = player.getVelocity();
         assertTrue("The player moves diagonally when W and D are pressed",
-                (vel.x>100&&vel.y>100));
-        assertEquals("The player moves diagonally at a speed of 200",
-                200, vel.len(), 5);
+                (vel.x>2&&vel.y>2));
+        assertEquals("The player moves diagonally at a speed of 4",
+                4, vel.len(), 0.05);
     }
     @Test
     public void testDiagonalMovementWA(){
@@ -78,9 +78,9 @@ public class PlayerTests {
         player.update(0.01f);
         Vector2 vel = player.getVelocity();
         assertTrue("The player moves diagonally when W and A are pressed",
-                (vel.x<-100&&vel.y>100));
-        assertEquals("The player moves diagonally at a speed of 200",
-                200, vel.len(), 5);
+                (vel.x<-2&&vel.y>2));
+        assertEquals("The player moves diagonally at a speed of 4",
+                4, vel.len(), 5);
     }
     @Test
     public void testDiagonalMovementSA(){
@@ -90,9 +90,9 @@ public class PlayerTests {
         player.update(0.01f);
         Vector2 vel = player.getVelocity();
         assertTrue("The player moves diagonally when S and A are pressed",
-                (vel.x<-100&&vel.y<-100));
-        assertEquals("The player moves diagonally at a speed of 200",
-                200, vel.len(), 5);
+                (vel.x<-2&&vel.y<-2));
+        assertEquals("The player moves diagonally at a speed of 4",
+                4, vel.len(), 0.05);
     }
     @Test
     public void testDiagonalMovementSD(){
@@ -102,9 +102,9 @@ public class PlayerTests {
         player.update(0.01f);
         Vector2 vel = player.getVelocity();
         assertTrue("The player moves diagonally when S and D are pressed",
-                (vel.x>100&&vel.y<-100));
-        assertEquals("The player moves diagonally at a speed of 200",
-                200, vel.len(), 5);
+                (vel.x>2 && vel.y<-2));
+        assertEquals("The player moves diagonally at a speed of 4",
+                4, vel.len(), 0.05);
     }
 
 
